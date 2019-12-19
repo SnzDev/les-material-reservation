a08140fa407d40c3c53a85ad2118222cc635c771
SnzDev <tms.working@gmail.com>
2019-12-18 22:56:31 -0300

    Configured database to heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-    username: 'root',
-    password: '',
-    database: 'les-material-reservation',
-    host: '127.0.0.1',
+    username: 'b9e30e8ba2eeff',
+    password: 'f7198292',
+    database: 'heroku_0bf6d39277c9b51',
+    host: 'us-cdbr-iron-east-05.cleardb.net',
     dialect: 'mysql',
   }
