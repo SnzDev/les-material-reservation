800e0b99df17406940392bab6a7caac71c54adcf
SnzDev <tms.working@gmail.com>
2019-12-18 22:40:53 -0300

    Added dependencie cors Added port to deploy heroku

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const routes = require('./routes');
-require("./database/index");
+const express = require("express");
 const app = express();
+var cors = require("cors");
+require("./database/index");
+const routes = require("./routes");
+
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT||3333);
